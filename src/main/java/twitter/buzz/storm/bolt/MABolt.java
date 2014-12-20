4d43cb6dc85b795400e54eae200d27ca5f91d095
yuya <yuya@fuji-PC>
2014-12-20 23:56:50 +0900

    add MABolt and Test

---
+++ src/main/java/twitter/buzz/storm/bolt/MABolt.java
@@ -0,0 +1,77 @@
+package twitter.buzz.storm.bolt;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import net.moraleboost.mecab.Lattice;
+import net.moraleboost.mecab.Node;
+import net.moraleboost.mecab.impl.StandardTagger;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+/**
+ * 形態素解析し名詞を抽出するためのBolt.
+ * @author yuya
+ *
+ */
+public class MABolt extends BaseRichBolt {
+
+    /** collector. */
+    private OutputCollector collector;
+
+    @Override
+    public final void prepare(final Map conf,
+                        final TopologyContext context,
+                        final OutputCollector oc) {
+        collector = oc;
+    }
+
+    @Override
+    public final void execute(final Tuple tuple) {
+        List<String> tupleData = (ArrayList<String>) tuple.getValue(0);
+        String timestampMs = tupleData.get(0);
+        String text = tupleData.get(1);
+        String screenName = tupleData.get(2);
+        List<String> nodeList = new ArrayList<String>();
+        // Taggerを構築。
+        // 引数には、MeCabのcreateTagger()関数に与える引数を与える。
+        StandardTagger tagger = new StandardTagger("");
+        // Lattice（形態素解析に必要な実行時情報が格納されるオブジェクト）を構築
+        Lattice lattice = tagger.createLattice();
+        // 解析対象文字列をセット
+        lattice.setSentence(text);
+        // tagger.parse()を呼び出して、文字列を形態素解析する。
+        tagger.parse(lattice);
+        // 一つずつ形態素をたどりながら、表層形と素性を出力
+        Node node = lattice.bosNode();
+        while (node != null) {
+            String surface = node.surface();
+            String feature = node.feature();
+            String[] hinshi = feature.split(",");
+            if (hinshi[0].equals("名詞")
+                    && (hinshi[1].equals("一般") || hinshi[1].equals("固有名詞"))
+                    && !hinshi[6].equals("*")
+                   ) {
+                collector.emit(new Values(surface));
+            }
+            node = node.next();
+        }
+
+        // lattice, taggerを破壊
+        lattice.destroy();
+        tagger.destroy();
+        collector.ack(tuple);
+    }
+
+    @Override
+    public final void declareOutputFields(final OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("ma"));
+    }
+
+}
---
+++ src/test/java/twitter/buzz/storm/bolt/MABoltTest.java
@@ -0,0 +1,62 @@
+package twitter.buzz.storm.bolt;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class MABoltTest {
+
+    /** MABolt. */
+    private MABolt mabolt;
+
+    /** collector. */
+    private OutputCollector collector;
+
+    @Before
+    public void setUp() throws Exception {
+        mabolt = new MABolt();
+        collector = mock(OutputCollector.class);
+        mabolt.prepare(new HashMap(), mock(TopologyContext.class), collector);
+    }
+
+    /**
+     * Textに名詞がある場合のテスト.
+     */
+    @Test
+    public void maBoltTest() {
+        List<String> data = new ArrayList<String>();
+        data.add("1415059203664");
+        data.add("日本人として知っておきたい"
+                + " \"おいしいお茶の入れ方\" http://t.co/wAwUGH93KB");
+        data.add("jyosi_up_1");
+        String expectedNode1 = "日本人";
+        String expectedNode2 = "お茶";
+        Tuple tuple = mock(Tuple.class);
+        when(tuple.getValue(0)).thenReturn(data);
+        mabolt.execute(tuple);
+        ArgumentCaptor<Values> args = ArgumentCaptor.forClass(Values.class);
+        verify(collector, times(2)).emit(args.capture());
+        List<Values> nodes = args.getAllValues();
+        String actualNode1 = nodes.get(0).get(0).toString();
+        String actualNode2 = nodes.get(1).get(0).toString();
+        assertThat(actualNode1, is(expectedNode1));
+        assertThat(actualNode2, is(expectedNode2));
+    }
+
+}
