28eecb04a95a07a6bed7d275a8d56d4bace017bb
yuya <yuya@fuji-PC>
2014-12-23 13:15:44 +0900

    add WordCountBolt and Test

---
+++ src/main/java/twitter/buzz/storm/bolt/WordCountBolt.java
@@ -0,0 +1,59 @@
+package twitter.buzz.storm.bolt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+/**
+ * 単語の出現回数を計算するBolt.
+ * @author yuya
+ *
+ */
+public class WordCountBolt extends BaseRichBolt {
+
+    /** collector. */
+    private OutputCollector collector;
+
+    /** Key:Word Value:CountのMap. */
+    private Map<String, Integer> counters;
+
+    @Override
+    public final void prepare(final Map stormConf,
+            final TopologyContext context,
+            final OutputCollector collector) {
+        this.counters = new HashMap<String, Integer>();
+        this.collector = collector;
+    }
+
+    @Override
+    public final void execute(final Tuple tuple) {
+        String word = tuple.getStringByField("word");
+        Integer cnt = counters.get(word);
+        if (cnt == null) {
+            cnt = 0;
+        }
+        cnt++;
+        counters.put(word, cnt);
+        collector.emit(new Values(word, cnt));
+        collector.ack(tuple);
+    }
+
+    @Override
+    public final void cleanup() {
+        for (Map.Entry<String, Integer> entry:counters.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    @Override
+    public final void declareOutputFields(final OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word", "cnt"));
+    }
+
+}
---
+++ src/test/java/twitter/buzz/storm/bolt/WordCountBoltTest.java
@@ -0,0 +1,74 @@
+package twitter.buzz.storm.bolt;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class WordCountBoltTest {
+
+    /** WordCountBolt. */
+    private WordCountBolt wcBolt;
+
+    /** collector. */
+    private OutputCollector collector;
+
+    @Before
+    public void setUp() throws Exception {
+        wcBolt = new WordCountBolt();
+        collector = mock(OutputCollector.class);
+        wcBolt.prepare(new HashMap(), mock(TopologyContext.class), collector);
+    }
+
+    @Test
+    public void wordCountTest() {
+        Tuple tuple = mock(Tuple.class);
+        List<String> wordList = new ArrayList<String>();
+        String expectedWord1 = "日本";
+        String expectedWord2 = "アメリカ";
+        wordList.add(expectedWord1);
+        wordList.add(expectedWord2);
+        wordList.add(expectedWord1);
+
+        // Tupleにこれら2つの単語を流す．
+        for (String word:wordList) {
+            when(tuple.getStringByField("word")).thenReturn(word);
+            wcBolt.execute(tuple);
+        }
+        ArgumentCaptor<Values> args = ArgumentCaptor.forClass(Values.class);
+        verify(collector, times(3)).emit(args.capture());
+
+        // extract actual values
+        String actualWord1 = args.getAllValues().get(0).get(0).toString();
+        Integer actualCnt1 = (Integer) args.getAllValues().get(0).get(1);
+        String actualWord2 = args.getAllValues().get(1).get(0).toString();
+        Integer actualCnt2 = (Integer) args.getAllValues().get(1).get(1);
+        String actualWord3 = args.getAllValues().get(2).get(0).toString();
+        Integer actualCnt3 = (Integer) args.getAllValues().get(2).get(1);
+
+        // assert
+        assertThat(actualWord1, is(expectedWord1));
+        assertThat(actualCnt1, is(1));
+        assertThat(actualWord2, is(expectedWord2));
+        assertThat(actualCnt2, is(1));
+        assertThat(actualWord3, is(expectedWord1));
+        assertThat(actualCnt3, is(2));
+    }
+
+}
