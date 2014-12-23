9a8bf361e70f406c0d84c6cd7f29e20f21a1950b
yuya <yuya@fuji-PC>
2014-12-23 13:15:26 +0900

    refactoring

--- src/test/java/twitter/buzz/storm/bolt/MABoltTest.java
+++ src/test/java/twitter/buzz/storm/bolt/MABoltTest.java
@@ -40,23 +40,26 @@ public class MABoltTest {
      */
     @Test
     public void maBoltTest() {
-        List<String> data = new ArrayList<String>();
         String text = "日本人として知っておきたい"
                 + " \"おいしいお茶の入れ方\" http://t.co/wAwUGH93KB";
         String screenName = "jyosi_up_1";
         String timestampMs = "1415059203664";
         String expectedNode1 = "日本人";
         String expectedNode2 = "お茶";
+
         Tuple tuple = mock(Tuple.class);
         when(tuple.getStringByField("timestamp_ms")).thenReturn(timestampMs);
         when(tuple.getStringByField("text")).thenReturn(text);
         when(tuple.getStringByField("screenName")).thenReturn(screenName);
         mabolt.execute(tuple);
+
         ArgumentCaptor<Values> args = ArgumentCaptor.forClass(Values.class);
         verify(collector, times(2)).emit(args.capture());
         List<Values> nodes = args.getAllValues();
+
         String actualNode1 = nodes.get(0).get(0).toString();
         String actualNode2 = nodes.get(1).get(0).toString();
+
         assertThat(actualNode1, is(expectedNode1));
         assertThat(actualNode2, is(expectedNode2));
     }
